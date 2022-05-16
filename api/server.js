ae7d1a3521f32bfc2e2290ad08a1049903ab2d61
laurensruiz <laurenruiz97@yahoo.com>
2022-05-16 13:05:29 -0400

    enpoints wired

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const accountsRouter = require('./accounts/accounts-router')
 const server = express();
 
 server.use(express.json());
+
+//must be put before catch all, order matters! and after express json because it needs to use it
+server.use('/api/accounts', accountsRouter)
 
 //catch all endpoint
 server.use('*', (req, res) => {
